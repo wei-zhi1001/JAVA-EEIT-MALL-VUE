73052c435ad6e11c0413b69f210e02df7a9db923
wujungen <wujungen1008@gmail.com>
2024-04-15 11:51:07 +0800

    cart&checkout+mall

--- src/views/product_detail_page.vue
+++ src/views/product_detail_page.vue
@@ -1,42 +1,46 @@
 <template >
-  <div class="modal fade" id="productView" tabindex="-1">
-    <div class="modal-dialog modal-lg modal-dialog-centered">
-      <div class="modal-content overflow-hidden border-0">
-        <button class="btn-close p-4 position-absolute top-0 end-0 z-index-20 shadow-0" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
-        <div class="modal-body p-0">
-          <div class="row align-items-stretch">
-            <div class="col-lg-6 p-lg-0"><a class="glightbox product-view d-block h-100 bg-cover bg-center" style="background: url( ../assets/img/product-5.jpg)" href="../assets/img/product-5.jpg" data-gallery="gallery1" data-glightbox="Red digital smartwatch"></a><a class="glightbox d-none" href="../assets/img/product-5-alt-1.jpg" data-gallery="gallery1" data-glightbox="Red digital smartwatch"></a><a class="glightbox d-none" href=" ../assets/img/product-5-alt-2.jpg" data-gallery="gallery1" data-glightbox="Red digital smartwatch"></a></div>
-            <div class="col-lg-6">
-              <div class="p-4 my-md-4">
-                <ul class="list-inline mb-2">
-                  <li class="list-inline-item m-0"><i class="fas fa-star small text-warning"></i></li>
-                  <li class="list-inline-item m-0 1"><i class="fas fa-star small text-warning"></i></li>
-                  <li class="list-inline-item m-0 2"><i class="fas fa-star small text-warning"></i></li>
-                  <li class="list-inline-item m-0 3"><i class="fas fa-star small text-warning"></i></li>
-                  <li class="list-inline-item m-0 4"><i class="fas fa-star small text-warning"></i></li>
-                </ul>
-                <h2 class="h4">{{ reProductName }}</h2>
-                <p class="text-muted">{{ rePrice }}</p>
-                <p class="text-sm mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.</p>
-                <div class="row align-items-stretch mb-4 gx-0">
-                  <div class="col-sm-7">
-                    <div class="border d-flex align-items-center justify-content-between py-1 px-3"><span class="small text-uppercase text-gray mr-4 no-select">Quantity</span>
-                      <div class="quantity">
-                        <button class="dec-btn p-0"><i class="fas fa-caret-left"></i></button>
-                        <input class="form-control border-0 shadow-0 p-0" type="text" value="1">
-                        <button class="inc-btn p-0"><i class="fas fa-caret-right"></i></button>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="col-sm-5"><a class="btn btn-dark btn-sm w-100 h-100 d-flex align-items-center justify-content-center px-0" href="cart.html">Add to cart</a></div>
-                </div><a class="btn btn-link text-dark text-decoration-none p-0" href="#!"><i class="far fa-heart me-2"></i>Add to wish list</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+<!--  <div class="modal fade" id="productView" tabindex="-1">-->
+<!--    <div class="modal-dialog modal-lg modal-dialog-centered">-->
+<!--      <div class="modal-content overflow-hidden border-0">-->
+<!--        <button class="btn-close p-4 position-absolute top-0 end-0 z-index-20 shadow-0" type="button" data-bs-dismiss="modal" aria-label="Close"></button>-->
+<!--        <div class="modal-body p-0">-->
+<!--          <div class="row align-items-stretch">-->
+<!--            <div class="col-lg-6 p-lg-0"><a class="glightbox product-view d-block h-100 bg-cover bg-center" style="background: url( ../assets/img/product-5.jpg)" href="../assets/img/product-5.jpg" data-gallery="gallery1" data-glightbox="Red digital smartwatch"></a><a class="glightbox d-none" href="../assets/img/product-5-alt-1.jpg" data-gallery="gallery1" data-glightbox="Red digital smartwatch"></a><a class="glightbox d-none" href=" ../assets/img/product-5-alt-2.jpg" data-gallery="gallery1" data-glightbox="Red digital smartwatch"></a></div>-->
+<!--            <div class="col-lg-6">-->
+<!--              <div class="p-4 my-md-4">-->
+<!--                <ul class="list-inline mb-2">-->
+<!--                  <li class="list-inline-item m-0"><i class="fas fa-star small text-warning"></i></li>-->
+<!--                  <li class="list-inline-item m-0 1"><i class="fas fa-star small text-warning"></i></li>-->
+<!--                  <li class="list-inline-item m-0 2"><i class="fas fa-star small text-warning"></i></li>-->
+<!--                  <li class="list-inline-item m-0 3"><i class="fas fa-star small text-warning"></i></li>-->
+<!--                  <li class="list-inline-item m-0 4"><i class="fas fa-star small text-warning"></i></li>-->
+<!--                </ul>-->
+<!--                <h2 class="h4">{{ reProductName }}</h2>-->
+<!--                <p class="text-muted">{{ rePrice }}</p>-->
+<!--                <p class="text-sm mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.</p>-->
+<!--                <div class="row align-items-stretch mb-4 gx-0">-->
+<!--&lt;!&ndash;                  <div class="col-sm-5 pr-sm-0">&ndash;&gt;-->
+<!--&lt;!&ndash;                    <div class="quantity-container">&ndash;&gt;-->
+<!--&lt;!&ndash;                      <span class="quantity-label">Quantity</span>&ndash;&gt;-->
+<!--&lt;!&ndash;                      <button class="quantity-btn dec-btn" @click="decrement">-</button>&ndash;&gt;-->
+<!--&lt;!&ndash;                      <input class="quantity-input" type="number" v-model="product.quantity" min="1">&ndash;&gt;-->
+<!--&lt;!&ndash;                      <button class="quantity-btn inc-btn" @click="increment">+</button>&ndash;&gt;-->
+<!--&lt;!&ndash;                      <button class="add-to-cart-btn" @click.prevent="addToCart">Add to Cart</button>&ndash;&gt;-->
+<!--&lt;!&ndash;                    </div>&ndash;&gt;-->
+<!--&lt;!&ndash;                  </div>&ndash;&gt;-->
+
+
+<!--                  <div class="col-sm-5"><button class="btn btn-dark btn-sm w-100 h-100 d-flex align-items-center justify-content-center px-0" @click="addToCart">-->
+<!--                    Add to cart-->
+<!--                  </button></div>-->
+<!--                </div><a class="btn btn-link text-dark text-decoration-none p-0" href="#!"><i class="far fa-heart me-2"></i>Add to wish list</a>-->
+<!--              </div>-->
+<!--            </div>-->
+<!--          </div>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--  </div>-->
   <section class="py-5">
     <div class="container">
       <div class="row mb-5">
@@ -75,17 +79,18 @@
           <p class="text-muted lead">${{ rePrice }}</p>
           <p class="text-sm mb-4">{{ reProductDescription }}</p>
           <div class="row align-items-stretch mb-4">
-            <div class="col-sm-5 pr-sm-0">
-              <div class="border d-flex align-items-center justify-content-between py-1 px-3 bg-white border-white">
-                <span class="small text-uppercase text-gray mr-4 no-select">Quantity</span>
-                <div class="quantity">
-                  <button class="dec-btn p-0" @click="updateQuantity(item, item.quantity - 1)"><i class="fas fa-caret-left"></i></button>
-                  <input class="form-control border-0 shadow-0 p-0" type="text" v-model="item.quantity">
-                  <button class="inc-btn p-0" @click="updateQuantity(item, item.quantity + 1)"><i class="fas fa-caret-right"></i></button>
-                </div>
+
+<!--            <div class="col-sm-5 pr-sm-0">-->
+              <div class="quantity-container">
+                <span class="quantity-label">Quantity</span>
+                <button class="quantity-btn dec-btn" @click="decrement">-</button>
+                <input class="quantity-input" type="number" v-model="product.quantity" min="1">
+                <button class="quantity-btn inc-btn" @click="increment">+</button>
+                <button class="add-to-cart-btn" @click.prevent="addToCart">Add to Cart</button>
               </div>
-            </div>
-            <div class="col-sm-3 pl-sm-0"><a class="btn btn-dark btn-sm btn-block h-100 d-flex align-items-center justify-content-center px-0" href="cart.html">Add to cart</a></div>
+<!--            </div>-->
+
+
           </div><a class="text-dark p-0 mb-4 d-inline-block" href="#!"><i class="far fa-heart me-2"></i>Add to wish list</a><br>
           <ul class="list-unstyled small d-inline-block">
 
@@ -148,6 +153,7 @@
 </template>
 <script>
 import axios from "axios";
+import {useUserStore} from "@/stores/userStore.js";
 
 function injectSvgSprite(path) {
   var ajax = new XMLHttpRequest();
@@ -164,6 +170,16 @@ function injectSvgSprite(path) {
 export default {
   data() {
     return {
+      item: {
+        quantity: 1,
+        specId: null,
+      },
+
+      product: {
+        id: null,
+        quantity: 1,  // Default starting quantity
+        specId: ''
+      },
       reProductId: this.$route.query.reProductId,
       reProductName: this.$route.query.reProductName,
       rePrice: this.$route.query.rePrice,
@@ -172,41 +188,137 @@ export default {
       reSpecIds: this.$route.query.reSpecIds
     };
   },
-  mounted() {
-
-// axios.get(`http://localhost:8080/mall/products/${this.reProductId}`).then((rs) => {
-//   console.log(rs.data)
-//   this.reProductName = rs.data.name
-//   this.rePrice = rs.data.price
-//   this.rePhotoId = rs.data.photoId
-//   this.reProductDescription = rs.data.description
-// })
-
-
+  computed: {
+    // This computed property ensures that specId is reactive and updates correctly
+    computedSpecId() {
+      return this.reSpecIds && this.reSpecIds.length > 0 ? this.reSpecIds[0] : null;
+    }
   },
+  watch: {
+    // Whenever reSpecIds changes, update the product.specId reactively
+    computedSpecId(newSpecId) {
+      this.product.specId = newSpecId;
+    }
+  },
+
   methods: {
-    async updateQuantity(item, newQuantity) {
-      if (newQuantity < 1) {
-        alert('Quantity cannot be less than 1.');
-        return; // Prevent quantity from going below 1
+    increment() {
+      this.product.quantity++;
+    },
+    decrement() {
+      if (this.product.quantity > 1) {
+        this.product.quantity--;
       }
+    },
+    async addToCart() {
+      const store = useUserStore();
+      if (!store.isLoggedIn) {
+        alert('Please log in to add items to your cart.');
+        this.$router.push('/login');
+        return;
+      }
+
+      const payload = {
+        userId: store.userId,
+        specId: this.product.specId,
+        quantity: this.product.quantity
+
+      };
+
+      console.log('Sending to backend:', payload);
+
       try {
-        const response = await axios.post(`${this.API_URL}/cart/add`, null, {
-          params: {
-            specId: item.specId,  // Assuming each item has a specId
-            quantity: newQuantity  // Passing new quantity directly to the backend
-          }
-        });
-        item.quantity = response.data.quantity; // Update quantity with server response
+        const response = await axios.post('http://localhost:8080/mall/cart/add', payload);
+        console.log('Added to cart successfully:', response.data);
       } catch (error) {
-        console.error('Failed to update cart item:', error);
-        alert('Failed to update cart quantity. Please try again.');
+        console.error('Failed to add to cart:', error);
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.error('Response data:', error.response.data);
+          console.error('Response status:', error.response.status);
+        }
       }
     }
-  }
+  },
+  mounted() {
+    console.log(this.$route.query.reSpecIds); // Check what is received
+    // If reSpecIds is received as a string, you might need to parse it:
+    if (typeof this.$route.query.reSpecIds === 'string') {
+      this.product.specId = JSON.parse(this.$route.query.reSpecIds)[0];
+    } else {
+      this.product.specId = this.$route.query.reSpecIds[0];
+    }
+  },
 }
 </script>
 
 <style>
+.quantity-container {
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+  background-color: white;
+  border: 1px solid #dee2e6; /* You can change this color to match your design */
+  display: flex;
+  align-items: center;
+}
+
+.quantity-label {
+  margin-right: 0.75rem;
+  text-transform: uppercase;
+  color: #6c757d; /* This is a gray color, adjust as needed */
+}
+
+.quantity-btn {
+  padding: 0.25rem 0.5rem;
+  border: 1px solid #dee2e6; /* Adjust border color as needed */
+  background-color: #f8f9fa; /* Light background for the button, change as needed */
+  cursor: pointer;
+
+}
+
+.dec-btn {
+  /* Styles for decrement button, if needed */
+}
+
+.inc-btn {
+  /* Styles for increment button, if needed */
+}
+
+.quantity-input {
+  width: 3rem; /* Adjust width as needed */
+  text-align: center;
+  margin: 0 0.5rem;
+  border: 1px solid #ced4da; /* Input border color, adjust as needed */
+}
+/* For Chrome, Safari, Edge, Opera */
+.quantity-input::-webkit-inner-spin-button,
+.quantity-input::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+/* For Firefox */
+.quantity-input[type=number] {
+  -moz-appearance: textfield;
+}
+.add-to-cart-btn {
+  flex-grow: 1; /* This will allow the button to take up the remaining space */
+  padding: 0.5rem 1rem;
+  color: white;
+  background-color: #586074; /* Adjust to match your primary button color */
+  border: none;
+  cursor: pointer;
+  margin-left: 16px; /* Adjust the left margin to create more space */
+  flex-grow: 1; /* This will make the button expand to fill available space */
+}
+
+/* Add responsive design if necessary */
+@media (max-width: 768px) {
+  .quantity-container {
+    /* Adjust styles for mobile if needed */
+  }
+}
+
 
 </style>
